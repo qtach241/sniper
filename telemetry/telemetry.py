fbbb203930e275f3ae56e87568b558bd48dee9b1
frank.he <frank.he@logicpd.com>
2021-12-09 01:53:57 -0600

    Remove tabs in python scripts.

--- telemetry/telemetry.py
+++ telemetry/telemetry.py
@@ -27,26 +27,26 @@ for opt, arg in opts:
     elif opt in ("--help"):
         print(usage_text)
         sys.exit()
-		
+
 class CbproWsClient(cbpro.WebsocketClient):
-	def on_open(self):
-		#self.url = "wss://ws-feed-public.sandbox.exchange.coinbase.com"
-		self.url = "wss://ws-feed.pro.coinbase.com/"
-		#self.url = "wss://ws-feed.exchange.coinbase.com"
-		#self.products = ["BTC-USD"]
-		#self.channels = ["ticker"]
-		self.message_count = 0
-		print("Let's count the messages!")
-	
-	def on_message(self, msg):
-		print(json.dumps(msg, indent=4, sort_keys=True))
-		self.message_count += 1
-		msg_type = msg.get('type', None)
-		print("msg_type: ", msg_type, self.message_count)
-	
-	def on_close(self):
-		print("-- Goodbye! --")
-		sys.exit(0)
+    def on_open(self):
+        #self.url = "wss://ws-feed-public.sandbox.exchange.coinbase.com"
+        self.url = "wss://ws-feed.pro.coinbase.com/"
+        #self.url = "wss://ws-feed.exchange.coinbase.com"
+        #self.products = ["BTC-USD"]
+        #self.channels = ["ticker"]
+        self.message_count = 0
+        print("Let's count the messages!")
+
+    def on_message(self, msg):
+        print(json.dumps(msg, indent=4, sort_keys=True))
+        self.message_count += 1
+        msg_type = msg.get('type', None)
+        print("msg_type: ", msg_type, self.message_count)
+
+    def on_close(self):
+        print("-- Goodbye! --")
+        sys.exit(0)
 
 def thread_loop():
     while True:
@@ -82,18 +82,18 @@ stats = public_client.get_product_24hr_stats("SOL-USD")
 #print(stats)
 
 try:
-	while True:
-		#ticker = public_client.get_product_ticker(product_id="SOL-USD")
-		#pprint.pprint(ticker)
-		#time.sleep(5)
-		#print("\nMessageCount =", "%i \n" % wsClient.message_count)
-		time.sleep(1)
+    while True:
+        #ticker = public_client.get_product_ticker(product_id="SOL-USD")
+        #pprint.pprint(ticker)
+        #time.sleep(5)
+        #print("\nMessageCount =", "%i \n" % wsClient.message_count)
+        time.sleep(1)
 except KeyboardInterrupt:
-	wsClient.close()
-	
+    wsClient.close()
+
 #if wsClient.error:
-#	sys.exit(1)
+    #sys.exit(1)
 #else:
-#	sys.exit(0)
+    #sys.exit(0)
 
 #thread.join()
