6ab54d60326b1f829cf374d9e9b219b111d2034d
frank.he <frank.he@logicpd.com>
2021-12-11 23:28:57 -0600

    Implement commands using the cbpro authenticated client.

--- telemetry/cbpro_console.py
+++ telemetry/cbpro_console.py
@@ -297,6 +297,421 @@ def get_time():
     print(json.dumps(api_time, indent=4, sort_keys=True))
 
 @cli.command()
+@click.option('--sandbox/--no-sandbox', default=True, help='Target sandbox or live API (default is sandbox)')
+def auth_get_accounts(sandbox):
+    """ Get a list of all trading accounts.
+
+    When you place an order, the funds for the order are placed on
+    hold. They cannot be used for other orders or withdrawn. Funds
+    will remain on hold until the order is filled or canceled. The
+    funds on hold for each account will be specified.
+
+    \b
+    Returns:
+        list: Info about all accounts. Example::
+            [
+                {
+                    "id": "71452118-efc7-4cc4-8780-a5e22d4baa53",
+                    "currency": "BTC",
+                    "balance": "0.0000000000000000",
+                    "available": "0.0000000000000000",
+                    "hold": "0.0000000000000000",
+                    "profile_id": "75da88c5-05bf-4f54-bc85-5c775bd68254"
+                },
+                {
+                    ...
+                }
+            ]
+    * Additional info included in response for margin accounts.
+
+    \b
+    Coinbase API Documentation:
+    https://docs.cloud.coinbase.com/exchange/reference/exchangerestapi_getaccounts
+    """
+    if sandbox:
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass,
+                                            api_url="https://api-public.sandbox.pro.coinbase.com")
+    else:
+        print("WARNING: Connecting to Live Coinbase API!")
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+ 
+    accounts = auth_client.get_accounts()
+    print(json.dumps(accounts, indent=4, sort_keys=True))
+
+@cli.command()
+@click.option('--sandbox/--no-sandbox', default=True, help='Target sandbox or live API (default is sandbox)')
+@click.option('--account_id', prompt='Enter Account Id', help='Account Id to get history of')
+def auth_get_account_history(sandbox, account_id):
+    """ List account activity (ledger). Account activity either increases or
+    decreases your account balance.
+
+    \b
+    Entry type indicates the reason for the account change.
+    * transfer:	Funds moved to/from Coinbase to cbpro
+    * match:	Funds moved as a result of a trade
+    * fee:	    Fee as a result of a trade
+    * rebate:   Fee rebate as per our fee schedule
+
+    \b
+    If an entry is the result of a trade (match, fee), the details
+    field will contain additional information about the trade.
+
+    \b
+    Returns:
+        list: History information for the account. Example::
+            [
+                {
+                    "id": "100",
+                    "created_at": "2014-11-07T08:19:27.028459Z",
+                    "amount": "0.001",
+                    "balance": "239.669",
+                    "type": "fee",
+                    "details": {
+                        "order_id": "d50ec984-77a8-460a-b958-66f114b0de9b",
+                        "trade_id": "74",
+                        "product_id": "BTC-USD"
+                    }
+                },
+                {
+                    ...
+                }
+            ]
+
+    \b
+    Coinbase API Documentation:
+    https://docs.cloud.coinbase.com/exchange/reference/exchangerestapi_getaccountledger
+    """
+
+    if sandbox:
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass,
+                                            api_url="https://api-public.sandbox.pro.coinbase.com")
+    else:
+        print("WARNING: Connecting to Live Coinbase API!")
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+
+    # Returns a generator obj
+    g_account_history = auth_client.get_account_history(account_id)
+
+    # Iterate through generator obj
+    for entry in g_account_history:
+        print(json.dumps(entry, indent=4, sort_keys=True))
+
+@cli.command()
+@click.option('--sandbox/--no-sandbox', default=True, help='Target sandbox or live API (default is sandbox)')
+@click.option('--account_id', prompt='Enter Account Id', help='Account Id to get history of')
+def auth_get_account_holds(sandbox, account_id):
+    """ Get holds on an account.
+    
+    This method returns a generator which may make multiple HTTP requests
+    while iterating through it.
+
+    Holds are placed on an account for active orders or
+    pending withdraw requests.
+
+    As an order is filled, the hold amount is updated. If an order
+    is canceled, any remaining hold is removed. For a withdraw, once
+    it is completed, the hold is removed.
+
+    The "type" field will indicate why the hold exists. The hold
+    type is "order" for holds related to open orders and 'transfer'
+    for holds related to a withdraw.
+
+    The "ref" field contains the id of the order or transfer which
+    created the hold.
+    
+    \b
+    Returns:
+        generator(list): Hold information for the account. Example::
+            [
+                {
+                    "id": "82dcd140-c3c7-4507-8de4-2c529cd1a28f",
+                    "account_id": "e0b3f39a-183d-453e-b754-0c13e5bab0b3",
+                    "created_at": "2014-11-06T10:34:47.123456Z",
+                    "updated_at": "2014-11-06T10:40:47.123456Z",
+                    "amount": "4.23",
+                    "type": "order",
+                    "ref": "0a205de4-dd35-4370-a285-fe8fc375a273",
+                },
+                {
+                ...
+                }
+            ]
+    
+    \b
+    Coinbase API Documentation:
+    https://docs.cloud.coinbase.com/exchange/reference/exchangerestapi_getaccountholds
+    """
+
+    if sandbox:
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass,
+                                            api_url="https://api-public.sandbox.pro.coinbase.com")
+    else:
+        print("WARNING: Connecting to Live Coinbase API!")
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+
+    # Returns a generator obj
+    g_account_holds = auth_client.get_account_holds(account_id)
+
+    # Iterate through generator obj
+    for entry in g_account_holds:
+        print(json.dumps(entry, indent=4, sort_keys=True))
+
+@cli.command()
+@click.option('--sandbox/--no-sandbox', default=True, help='Target sandbox or live API (default is sandbox)')
+@click.option('--product', prompt='Enter Product Id', help='Product Id (ie. BTC-USD)')
+@click.option('--side', prompt='Enter buy or sell', help='Order side (buy or sell)')
+@click.option('--price', prompt='Enter price', help='Price per cryptocurrency')
+@click.option('--size', prompt='Enter size', help='Amount of cryptocurrency to buy or sell')
+def auth_place_limit_order(sandbox, product, side, price, size):
+    """Place a limit order.
+    
+    \b
+    Coinbase API Documentation:
+    https://docs.cloud.coinbase.com/exchange/reference/exchangerestapi_postorders
+    """
+    
+    if sandbox:
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass,
+                                            api_url="https://api-public.sandbox.pro.coinbase.com")
+    else:
+        print("WARNING: Connecting to Live Coinbase API!")
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+
+    # Limit order specific method
+    ret = auth_client.place_limit_order(product_id=product,
+                                        side=side,
+                                        price=price,
+                                        size=size)
+    # Print result code
+    print(json.dumps(ret, indent=4, sort_keys=True))
+
+@cli.command()
+@click.option('--sandbox/--no-sandbox', default=True, help='Target sandbox or live API (default is sandbox)')
+@click.option('--product', prompt='Enter Product Id', help='Product Id (ie. BTC-USD)')
+@click.option('--side', prompt='Enter buy or sell', help='Order side (buy or sell)')
+@click.option('--funds', default=None, help='(Optional) Desired amount of quote currency to use. Specify this or "size"')
+@click.option('--size', default=None, help='(Optional) Desired amount of cryptocurrency. Specify this or "funds"')
+def auth_place_market_order(sandbox, product, side, funds, size):
+    """Place a market order.
+    
+    \b
+    Coinbase API Documentation:
+    https://docs.cloud.coinbase.com/exchange/reference/exchangerestapi_postorders
+    """
+
+    if funds is None and size is None:
+        print("Must provide either --funds or --size options")
+        return
+    
+    if sandbox:
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass,
+                                            api_url="https://api-public.sandbox.pro.coinbase.com")
+    else:
+        print("WARNING: Connecting to Live Coinbase API!")
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+
+    # Market order specific method
+    ret = auth_client.place_market_order(product_id=product,
+                                        side=side,
+                                        funds=funds,
+                                        size=size)
+    # Print result code
+    print(json.dumps(ret, indent=4, sort_keys=True))
+
+@cli.command()
+@click.option('--sandbox/--no-sandbox', default=True, help='Target sandbox or live API (default is sandbox)')
+@click.option('--order_id', prompt='Enter Order Id', help='The order_id of the order to cancel')
+def auth_cancel_order(sandbox, order_id):
+    """ Cancel a previously placed order.
+
+    If the order had no matches during its lifetime its record may
+    be purged. This means the order details will not be available
+    with get_order(order_id). If the order could not be canceled
+    (already filled or previously canceled, etc), then an error
+    response will indicate the reason in the message field.
+
+    **Caution**: The order id is the server-assigned order id and
+    not the optional client_oid.
+
+    \b
+    Returns:
+        list: Containing the order_id of cancelled order. Example::
+            [ "c5ab5eae-76be-480e-8961-00792dc7e138" ]
+
+    \b
+    Coinbase API Documentation:
+    https://docs.cloud.coinbase.com/exchange/reference/exchangerestapi_deleteorder
+    """
+
+    if sandbox:
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass,
+                                            api_url="https://api-public.sandbox.pro.coinbase.com")
+    else:
+        print("WARNING: Connecting to Live Coinbase API!")
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+
+    ret = auth_client.cancel_order(order_id)
+    print(json.dumps(ret, indent=4, sort_keys=True))
+
+@cli.command()
+@click.option('--sandbox/--no-sandbox', default=True, help='Target sandbox or live API (default is sandbox)')
+@click.option('--product', default=None, help='(Optional) Only cancel orders for this product')
+def auth_cancel_all(sandbox, product):
+    """Attempt to cancel all orders
+    
+    \b
+    Coinbase API Documentation:
+    https://docs.cloud.coinbase.com/exchange/reference/exchangerestapi_deleteorders
+    """
+
+    if sandbox:
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass,
+                                            api_url="https://api-public.sandbox.pro.coinbase.com")
+    else:
+        print("WARNING: Connecting to Live Coinbase API!")
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+
+    ret = auth_client.cancel_all(product_id=product)
+    print(json.dumps(ret, indent=4, sort_keys=True))
+
+@cli.command()
+@click.option('--sandbox/--no-sandbox', default=True, help='Target sandbox or live API (default is sandbox)')
+def auth_get_orders(sandbox):
+    """ List your current open orders.
+
+    This method returns a generator which may make multiple HTTP requests
+    while iterating through it.
+
+    Only open or un-settled orders are returned. As soon as an
+    order is no longer open and settled, it will no longer appear
+    in the default request.
+
+    Orders which are no longer resting on the order book, will be
+    marked with the 'done' status. There is a small window between
+    an order being 'done' and 'settled'. An order is 'settled' when
+    all of the fills have settled and the remaining holds (if any)
+    have been removed.
+
+    For high-volume trading it is strongly recommended that you
+    maintain your own list of open orders and use one of the
+    streaming market data feeds to keep it updated. You should poll
+    the open orders endpoint once when you start trading to obtain
+    the current state of any open orders.
+    
+    \b
+    Returns:
+        list: Containing information on orders. Example::
+            [
+                {
+                    "id": "d0c5340b-6d6c-49d9-b567-48c4bfca13d2",
+                    "price": "0.10000000",
+                    "size": "0.01000000",
+                    "product_id": "BTC-USD",
+                    "side": "buy",
+                    "stp": "dc",
+                    "type": "limit",
+                    "time_in_force": "GTC",
+                    "post_only": false,
+                    "created_at": "2016-12-08T20:02:28.53864Z",
+                    "fill_fees": "0.0000000000000000",
+                    "filled_size": "0.00000000",
+                    "executed_value": "0.0000000000000000",
+                    "status": "open",
+                    "settled": false
+                },
+                {
+                    ...
+                }
+            ]
+
+    \b
+    Coinbase API Documentation:
+    https://docs.cloud.coinbase.com/exchange/reference/exchangerestapi_getorders
+    """
+
+    if sandbox:
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass,
+                                            api_url="https://api-public.sandbox.pro.coinbase.com")
+    else:
+        print("WARNING: Connecting to Live Coinbase API!")
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+
+    # Returns a generator obj
+    g_orders = auth_client.get_orders()
+
+    # Iterate through generator obj
+    for entry in g_orders:
+        print(json.dumps(entry, indent=4, sort_keys=True))
+
+@cli.command()
+@click.option('--sandbox/--no-sandbox', default=True, help='Target sandbox or live API (default is sandbox)')
+@click.option('--product', default=None, help='Limit list to this product id')
+@click.option('--order_id', default=None, help='Limit list to this order id')
+def auth_get_fills(sandbox, product, order_id):
+    """ Get a list of recent fills.
+
+    As of 8/23/18 - Requests without either order_id or product_id
+    will be rejected.
+
+    This method returns a generator which may make multiple HTTP requests
+    while iterating through it.
+
+    Fees are recorded in two stages. Immediately after the matching
+    engine completes a match, the fill is inserted into our
+    datastore. Once the fill is recorded, a settlement process will
+    settle the fill and credit both trading counterparties.
+
+    The 'fee' field indicates the fees charged for this fill.
+
+    The 'liquidity' field indicates if the fill was the result of a
+    liquidity provider or liquidity taker. M indicates Maker and T
+    indicates Taker.
+
+    \b
+    Returns:
+        list: Containing information on fills. Example::
+            [
+                {
+                    "trade_id": 74,
+                    "product_id": "BTC-USD",
+                    "price": "10.00",
+                    "size": "0.01",
+                    "order_id": "d50ec984-77a8-460a-b958-66f114b0de9b",
+                    "created_at": "2014-11-07T22:19:28.578544Z",
+                    "liquidity": "T",
+                    "fee": "0.00025",
+                    "settled": true,
+                    "side": "buy"
+                },
+                {
+                    ...
+                }
+            ]
+
+    \b
+    Coinbase API Documentation:
+    https://docs.cloud.coinbase.com/exchange/reference/exchangerestapi_getfills
+    """
+
+    if product is None and order_id is None:
+        print("Must provide either --product or --order_id options")
+        return
+
+    if sandbox:
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass,
+                                            api_url="https://api-public.sandbox.pro.coinbase.com")
+    else:
+        print("WARNING: Connecting to Live Coinbase API!")
+        auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+
+    # Returns a generator obj
+    g_fills = auth_client.get_fills(product_id=product, order_id=order_id)
+
+    # Iterate through generator obj
+    for entry in g_fills:
+        print(json.dumps(entry, indent=4, sort_keys=True))
+
+@cli.command()
 @click.option('--product', prompt='Enter Product Id', help='Product Id (ie. BTC-USD)')
 @click.option('--expiry', default=60, help='Time (in seconds) to remain subscribed')
 def level3_order_book(product, expiry):
