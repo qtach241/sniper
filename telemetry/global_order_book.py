f29a489366c8768b17e2689021ce02f282f2c956
frank.he <frank.he@logicpd.com>
2021-12-15 02:16:11 -0600

    Refactor json structure to store from global order book.

--- telemetry/global_order_book.py
+++ telemetry/global_order_book.py
@@ -4,7 +4,7 @@ import logging
 import os
 import socket
 import threading
-import json
+import simplejson as json
 import time
 import datetime as dt
 from auth_keys import (api_secret, api_key, api_pass)
@@ -21,29 +21,91 @@ from cbpro_level2_order_book import Cb_L2OrderBook
 if __name__ == '__main__':
     print("Started global order book at: ", dt.datetime.now())
     
-    # Start L2 order books.
-    coinbase_btc_usd = Cb_L2OrderBook(product_id='BTC-USD')
-    coinbase_eth_usd = Cb_L2OrderBook(product_id='ETH-USD')
-    coinbase_sol_usd = Cb_L2OrderBook(product_id='SOL-USD')
+    # Start Coinbase L2 order books.
+    Coinbase_BTC_USD = Cb_L2OrderBook(product_id='BTC-USD')
+    Coinbase_ETH_USD = Cb_L2OrderBook(product_id='ETH-USD')
+    Coinbase_SOL_USD = Cb_L2OrderBook(product_id='SOL-USD')
+    #Coinbase_MAT_USD = Cb_L2OrderBook(product_id='MATIC_USD')
     
-    coinbase_btc_usd.create()
-    coinbase_eth_usd.create()
-    coinbase_sol_usd.create()
+    Coinbase_BTC_USD.create()
+    Coinbase_ETH_USD.create()
+    Coinbase_SOL_USD.create()
+    #Coinbase_MAT_USD.create()
 
     # Initial time delay to finish building snapshot
-    time.sleep(2)
+    time.sleep(5)
 
     while True:
-        cb_btc_usd_book = coinbase_btc_usd.export()
-        cb_eth_usd_book = coinbase_eth_usd.export()
-        cb_sol_usd_book = coinbase_sol_usd.export()
 
-        print("btc_usd timestamp: ", coinbase_btc_usd.get_update_time())
-        print("eth_usd timestamp: ", coinbase_eth_usd.get_update_time())
-        print("sol_usd timestamp: ", coinbase_sol_usd.get_update_time())
-        print(cb_btc_usd_book)
-        print(cb_eth_usd_book)
-        print(cb_sol_usd_book)
+        current_time = dt.datetime.utcnow().isoformat()
+
+        Coinbase_BTC_USD_lut = Coinbase_BTC_USD.get_update_time()
+        Coinbase_BTC_USD_ask = Coinbase_BTC_USD.get_ask()
+        Coinbase_BTC_USD_bid = Coinbase_BTC_USD.get_bid()
+        Coinbase_BTC_USD_depth = Coinbase_BTC_USD.export()
+
+        Coinbase_ETH_USD_lut = Coinbase_ETH_USD.get_update_time()
+        Coinbase_ETH_USD_ask = Coinbase_ETH_USD.get_ask()
+        Coinbase_ETH_USD_bid = Coinbase_ETH_USD.get_bid()
+        Coinbase_ETH_USD_depth = Coinbase_ETH_USD.export()
+
+        Coinbase_SOL_USD_lut = Coinbase_SOL_USD.get_update_time()
+        Coinbase_SOL_USD_ask = Coinbase_SOL_USD.get_ask()
+        Coinbase_SOL_USD_bid = Coinbase_SOL_USD.get_bid()
+        Coinbase_SOL_USD_depth = Coinbase_SOL_USD.export()
+
+        #Coinbase_MAT_USD_lut = Coinbase_MAT_USD.get_update_time()
+        #Coinbase_MAT_USD_ask = Coinbase_MAT_USD.get_ask()
+        #Coinbase_MAT_USD_bid = Coinbase_MAT_USD.get_bid()
+        #Coinbase_MAT_USD_depth = Coinbase_MAT_USD.export()
+
+        data = {}
+        data['timestamp'] = current_time
+        
+        cb_data = {}
+
+        cb_btc_usd_data = {}
+        cb_btc_usd_data['last_update_at'] = Coinbase_BTC_USD_lut
+        cb_btc_usd_data['bid'] = Coinbase_BTC_USD_bid
+        cb_btc_usd_data['ask'] = Coinbase_BTC_USD_ask
+        cb_btc_usd_data['bid_depth'] = Coinbase_BTC_USD_depth[1].to_dict()
+        cb_btc_usd_data['ask_depth'] = Coinbase_BTC_USD_depth[0].to_dict()
+
+        cb_eth_usd_data = {}
+        cb_eth_usd_data['last_update_at'] = Coinbase_ETH_USD_lut
+        cb_eth_usd_data['bid'] = Coinbase_ETH_USD_bid
+        cb_eth_usd_data['ask'] = Coinbase_ETH_USD_ask
+        cb_eth_usd_data['bid_depth'] = Coinbase_ETH_USD_depth[1].to_dict()
+        cb_eth_usd_data['ask_depth'] = Coinbase_ETH_USD_depth[0].to_dict()
+
+        cb_sol_usd_data = {}
+        cb_sol_usd_data['last_update_at'] = Coinbase_SOL_USD_lut
+        cb_sol_usd_data['bid'] = Coinbase_SOL_USD_bid
+        cb_sol_usd_data['ask'] = Coinbase_SOL_USD_ask
+        cb_sol_usd_data['bid_depth'] = Coinbase_SOL_USD_depth[1].to_dict()
+        cb_sol_usd_data['ask_depth'] = Coinbase_SOL_USD_depth[0].to_dict()
+
+        #cb_mat_usd_data = {}
+        #cb_mat_usd_data['last_update_at'] = Coinbase_MAT_USD_lut
+        #cb_mat_usd_data['bid'] = Coinbase_MAT_USD_bid
+        #cb_mat_usd_data['ask'] = Coinbase_MAT_USD_ask
+        #cb_mat_usd_data['bid_depth'] = Coinbase_MAT_USD_depth[1].to_dict()
+        #cb_mat_usd_data['ask_depth'] = Coinbase_MAT_USD_depth[0].to_dict()
+
+        cb_data['BTC-USD'] = cb_btc_usd_data
+        cb_data['ETH-USD'] = cb_eth_usd_data
+        cb_data['SOL-USD'] = cb_sol_usd_data
+        #cb_data['MATIC-USD'] = cb_mat_usd_data
+
+        data['coinbase'] = cb_data
+
+        bids_df = pandas.DataFrame.from_dict(cb_sol_usd_data['bid_depth'])
+        asks_df = pandas.DataFrame.from_dict(cb_sol_usd_data['ask_depth'])
+        print(bids_df)
+        print(asks_df)
+
+        json_data = json.dumps(data)
+        print(json_data)
 
         time.sleep(1)
 
