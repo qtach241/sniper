d9d0d70b997898d6bffb98bab24fdf0f9c3132a8
frank.he <frank.he@logicpd.com>
2021-12-17 23:26:11 -0600

    Add rpi_setup.sh script. Switch to stretch instead of alpine.

---
+++ telemetry/rpi_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+sudo usermod -aG docker pi
+
+sudo apt-get install -y git pip
