449abf3dc1fb0bbb6d6cf6f21e97207ed10b71f5
frank.he <frank.he@logicpd.com>
2021-12-19 14:05:59 -0600

    Shorten depth window from 20% to 10%. This allows binance order books to be more accurate initially. 10% should be enough depth even for volatile coins.

--- telemetry/base_level2_order_book.py
+++ telemetry/base_level2_order_book.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from decimal import Decimal
 
-BIN_DEPTH_MARKER = [0.02, 0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18, 0.20]
+BIN_DEPTH_MARKER = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
 
 class L2OrderBook(ABC):
 
