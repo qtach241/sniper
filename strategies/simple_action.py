8223bf02f6c9e324e504334039f30aed6fb2201d
frank.he <frank.he@logicpd.com>
2021-12-25 15:11:47 -0600

    Make agent a class variable of the action class.

--- strategies/simple_action.py
+++ strategies/simple_action.py
@@ -3,10 +3,19 @@ from abc import ABC, abstractmethod
 
 class ActionSpace(Enum):
     HOLD = 1
-    BUY_ALL = 2
-    SELL_ALL = 3
+    MARKET_ORDER_BUY_ALL = 2
+    MARKET_ORDER_SELL_ALL = 3
+    MARKET_ORDER_BUY_TENTH = 4
+    MARKET_ORDER_SELL_TENTH = 5
 
 class Action(ABC):
+    def __init__(self, agent, id) -> None:
+        self._agent = agent
+        self._id = id
+
+    @property
+    def action_id(self) -> ActionSpace:
+        return id
 
     @abstractmethod
     def execute(self):
@@ -17,11 +26,13 @@ class Action(ABC):
         pass
 
 class Action_BuyAll(Action):
-
-    id = ActionSpace.BUY_ALL
+    def __init__(self, agent) -> None:
+        super().__init__(agent, id=ActionSpace.MARKET_ORDER_BUY_ALL)
 
     def execute(self):
         pass
 
     def simulate(self):
+        # Check the current state, if QTY of USD is greater than 0, transfer
+        # all USD to the crypto asset at the current bid.
         pass
